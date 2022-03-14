de46d06492bce6f6f92a1c02be8216ce317d8524
sharon-06 <71642896+sharon-06@users.noreply.github.com>
2022-03-14 11:39:09 +0530

    done

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
 class SortedList {
   constructor() {
-  
+    this.items = []
+    this.length = 0
   }
-  add() {
-   
-  }
-  get() {
 
+  add(x) {
+    this.items.push(x)
+    this.items.sort((a,b)=>a-b)
+    this.length = this.items.length
   }
+
+  get(i) {
+    if(this.items.indexOf(i) == -1){
+      throw new Error('OutOfBounds')
+    }
+    else{
+      return(this.items.indexOf(i))
+    }
+  }
+
   max() {
-
+    if(this.items.length >0){
+      return this.items[this.items.length - 1]
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
+
   min() {
-
+    if(this.items.length > 0){
+      return this.items[0]
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
+
+
   avg() {
-    
-   
+    if(this.items.length > 0){
+      return this.sum() / this.items.length
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
 
   sum() {
-  
+    if(this.items.length>0){
+      return this.items.reduce((acc, item)=> acc = acc + item, 0)
+    }
+    else{
+      return 0
+    }
   }
   
 };
